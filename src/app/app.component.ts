8d88ca4113b4a0aca44be686e581def5683f22f0
Xinhe YU <aspasia.yu@gmail.com>
2024-09-17 23:21:12 +0200

    pass from module to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
+import { HomeComponent } from './pages/home/home.component';
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  constructor(private olympicService: OlympicService) {}
+export class AppComponent {
 
-  ngOnInit(): void {
-    this.olympicService.loadInitialData().pipe(take(1)).subscribe();
-  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,15 +4,18 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+export class HomeComponent { }
+// export class HomeComponent implements OnInit {
+//   public olympics$: Observable<any> = of(null);
 
-  constructor(private olympicService: OlympicService) {}
+//   constructor(private olympicService: OlympicService) { }
 
-  ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-  }
-}
+//   ngOnInit(): void {
+//     this.olympics$ = this.olympicService.getOlympics();
+//   }
+// }
