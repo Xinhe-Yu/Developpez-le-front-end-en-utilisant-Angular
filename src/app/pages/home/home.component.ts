4bb3fef6d3ad793df199d922d52c91cab2f78c5f
Xinhe YU <aspasia.yu@gmail.com>
2024-09-27 22:28:51 +0200

    delete unnecessary rxjs observable and add unsubscription

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { AsyncPipe, NgIf } from '@angular/common';
@@ -19,11 +19,11 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 
-export class HomeComponent implements OnInit {
-  public olympics$!: Observable<Olympic[] | null>;
+export class HomeComponent implements OnInit, OnDestroy {
+  public olympics$!: Observable<Olympic[]>;
   svgIconPath: string = 'src/assets/mock/icons/medal-solid.svg';
   // ngx-charts config
-  chartData$!: Observable<PieChartData[]>;
+  chartData: PieChartData[] = [];
   gradient: boolean = false;
   showLegend: boolean = false;
   showLabels: boolean = true;
@@ -31,30 +31,41 @@ export class HomeComponent implements OnInit {
   maxLabelLength: number = 20;
   trimLabels: boolean = false;
   disable: boolean = false;
+  private olympicsSubscription!: Subscription;
 
   constructor(
     private olympicService: OlympicService,
-    private router: Router
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
-    this.chartData$ = this.olympics$.pipe(
-      map((olympics: Olympic[] | null) => this.transformData(olympics))
-    );
-  };
+
+    this.olympicsSubscription = this.olympics$.subscribe((olympics: Olympic[]) => {
+      this.chartData = this.getChartData(olympics);
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.olympicsSubscription && !this.olympicsSubscription.closed) {
+      this.olympicsSubscription.unsubscribe();
+    }
+  }
+  getChartData(olympics: Olympic[]): PieChartData[] {
+    const chartData = olympics.map(olympic => {
+      const totalMedals = olympic.participations.reduce((sum, p) => sum + p.medalsCount, 0);
+
+      return {
+        name: olympic.country,
+        value: totalMedals,
+        extra: { id: olympic.id }
+      };
+    });
+    return chartData;
+  }
 
   onSelect(event: PieChartData): void {
     const countryId = event.extra?.id;
     this.router.navigate(['/countries', countryId])
-  }
-
-  private transformData(olympics: Olympic[] | null): PieChartData[] {
-    if (!olympics) return [];
-    return olympics.map(olympic => ({
-      name: olympic.country,
-      value: olympic.participations.reduce((sum, p) => sum + p.medalsCount, 0),
-      extra: { id: olympic.id }
-    }));
   }
 }
